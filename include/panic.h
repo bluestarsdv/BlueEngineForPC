52fe395eeb3132f14891a27f1ce2616580545768
bluestarsdv <turmadoyeahblu3@gmail.com>
2026-07-25 13:38:03 -0300

    Update panic.h

--- include/panic.h
+++ include/panic.h
@@ -1,6 +1,13 @@
 #ifndef KERNEL_PANIC_H
 #define KERNEL_PANIC_H
 
+// Tipos inteiros manuais para garantir autonomia bare-metal
+typedef unsigned char      u8_t;
+typedef unsigned short     u16_t;
+typedef unsigned int       u32_t;
+typedef unsigned int       uintptr_t;
+
+// Função de Panic
 void panic(const char *message);
 
-#endif
+#endif // KERNEL_PANIC_H
